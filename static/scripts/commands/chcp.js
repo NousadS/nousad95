6ada61b7f345674e002e5c15676e1b53924d2eee
piwerm <m.buldakov10min@gmail.com>
2024-08-12 05:26:38 +0200

    update: chcp command

--- static/scripts/commands/chcp.js
+++ static/scripts/commands/chcp.js
@@ -1,7 +1,7 @@
 function chcp(args) {
     let response = "\n";
 
-    if (args.length == 1 && args[0] != "" && Object.keys(localization).includes(parseInt(args[0]))) {
+    if (args.length == 1 && args[0] != "" && Object.keys(localization).includes(args[0])) {
         sessionParameters.codePage = parseInt(args[0]);
 
         response += `${localization[sessionParameters.codePage].commands.chcp.active} ${sessionParameters.codePage}\n`;
